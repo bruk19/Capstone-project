85aa236654091a4dc0376744dceaeaf1c8c7d04a
bruk19 <brukteshome21@gmail.com>
2022-05-17 16:04:05 +0300

    Add js for mobile menu

---
+++ app.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('#menu-button');
+const closeMenuBtn = document.querySelector('i.close-menu');
+const header = document.querySelector('header');
+const horseCardSkeleton = document.querySelector('#horse-card-skeleton');
+const featuredHorses = document.querySelector('#featured-horses ul');
+const body = document.querySelector('body');
